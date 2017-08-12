b0bfde645a395be99f2186d6ca27197f398cd022
L3D <l3d@see-base.de>
2017-08-13 01:05:39 +0200

    RSS Parser 'funktioniert'

--- rss2html.ffbsee.pl
+++ rss2html.ffbsee.pl
@@ -5,6 +5,7 @@ use warnings;
 use LWP 5.64;
 use XML::Simple;
 use Data::Dumper;
+use HTML::Entities;
 
 # create object
 my $xml = new XML::Simple;
@@ -18,6 +19,21 @@ my $seite_code = $seite->decoded_content();
 my $data = $xml->XMLin("$seite_code");
 
 # print output
-print Dumper($data);
+#print Dumper($data);
+
+my $news_count=0;
+while (defined ($data->{"channel"}->{"item"}->[$news_count])){
+    $news_count++
+}
+# print $news_count; #wie viele news exestieren...
+
+open FH, ">", 'newsfeed.html' or die "Error writing 'newsfeed.html': $!\n";
+
+print FH encode_entities($data->{'channel'}->{'item'}->[$news_count-1]->{'description'}, '^\n\x20-\x25\x27-\x7e');
+print FH encode_entities($data->{'channel'}->{'item'}->[$news_count-2]->{'description'}, '^\n\x20-\x25\x27-\x7e');
+print FH encode_entities($data->{'channel'}->{'item'}->[$news_count-3]->{'description'}, '^\n\x20-\x25\x27-\x7e');
+
+close FH;
+
 
 
